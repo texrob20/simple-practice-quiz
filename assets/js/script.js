c3408d23f0bd9dc2a3054ef9e58aec5ef44a327c
Robert C <robert.clarady@gmail.com>
2021-11-19 13:19:22 -0600

    updated styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,43 +47,60 @@ var userScore = {
     user: "",
     finalScore: 0,
 };
+// action taken when answer selected
 var Clicked = function() {
     console.log(this.id, this.innerHTML);
     console.log(codeQuestions[counter].answer);
+    //check to see if answer is correct or not
     if (this.innerHTML == codeQuestions[counter].answer) {
         console.log ("add 10 points");
-        score +=10;
+        window.alert("Correct!");
+        score +=10; //adds 10 points to score
     } else {
         console.log ("subtract 10 sec");
-        timer -=10;
+        window.alert("Not correct.  The correct answer is " + codeQuestions[counter].answer);
+        timer -=10; //decucts 10 seconds from timer
     }
     console.log("counter " + counter);    
     counter ++;
+    // starts next question
     DisplayQuestion();
 }
 
+// timer function
 function displayTime(){
     if (timer > 0) {
     document.getElementById("timeLeft").innerText = timer;
     timer--; }
     else {
-    timer = "Out of time";
+    timer = "Quiz over";
     document.getElementById("timeLeft").innerText = timer;
     return;
     }
 
     console.log(timer);
 }
-function saveScore() {
-    //localStorage.setItem("score", JSON.stringify(score));
-    console.log("score saved")
+
+// once all questions answered, clears questions and saves score
+function Score() {
+    console.log("score saved");
+    var user = window.prompt ("Your score is " + score +". Please enter your name.");
     document.getElementById("title").innerHTML = "";
     document.getElementById("question").innerHTML = "";
+    document.getElementById("question").setAttribute("class", "hidden");
     document.getElementById("possAns1").innerHTML = "";
+    document.getElementById("possAns1").setAttribute("class", "hidden");
     document.getElementById("possAns2").innerHTML = "";
+    document.getElementById("possAns2").setAttribute("class", "hidden");
     document.getElementById("possAns3").innerHTML = "";
+    document.getElementById("possAns3").setAttribute("class", "hidden");
     document.getElementById("possAns4").innerHTML = "";
+    document.getElementById("possAns4").setAttribute("class", "hidden");
     timer=0;
+    function saveScore(){
+
+    }
+    console.log(user);
 };
 
 var loadScore = function() {
@@ -100,38 +117,47 @@ var loadScore = function() {
     }
 };
 
+// based on button click, starts quiz and timer
 function executeQuiz(){
     var time = setInterval(displayTime, 1000);
+    document.getElementById("quizInfo").setAttribute ("class", "hidden");
     document.getElementById("preQuizTitle").innerHTML = "";
     document.getElementById("info").innerHTML = "";
     document.getElementById("start").innerHTML = "";
+    document.getElementById("title").innerHTML = "Coding Quiz";
  
     DisplayQuestion();
 }
 
+// function runs through question set
 function DisplayQuestion () {
     if (counter === codeQuestions.length){
-       saveScore();
+       Score();
     } else {
         let displayQuestion = "";
         displayQuestion += codeQuestions[counter].question;
         document.getElementById("question").innerHTML = displayQuestion;
+        document.getElementById("question").setAttribute("class", "question");
         let option1 ="";
         option1 += codeQuestions[counter].option1;
         document.getElementById("possAns1").innerHTML = option1;
         document.getElementById("possAns1").onclick = Clicked;
+        document.getElementById("possAns1").setAttribute("class", "option");
         let option2 ="";
         option2 += codeQuestions[counter].option2;
         document.getElementById("possAns2").innerHTML = option2;
         document.getElementById("possAns2").onclick = Clicked;
+        document.getElementById("possAns2").setAttribute("class", "option");
         let option3 ="";
         option3 += codeQuestions[counter].option3;
         document.getElementById("possAns3").innerHTML = option3;
         document.getElementById("possAns3").onclick = Clicked;
+        document.getElementById("possAns3").setAttribute("class", "option");
         let option4 ="";
         option4 += codeQuestions[counter].option4;
         document.getElementById("possAns4").innerHTML = option4;
         document.getElementById("possAns4").onclick = Clicked;
+        document.getElementById("possAns4").setAttribute("class", "option");
 }};
  
 
